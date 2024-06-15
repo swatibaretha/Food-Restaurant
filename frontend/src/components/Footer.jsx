066928833aa84ee67ea2b94b496e28522abe9c1e
swatibaretha <145223587+swatibaretha@users.noreply.github.com>
2024-06-15 10:28:21 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Foddu</div>
+          <div className="right">
+            <p>FD Gandhi Nagar Indore(m.p.)</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Swati</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Swati.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
